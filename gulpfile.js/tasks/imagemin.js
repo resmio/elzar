bd37333a754f9e0328902c600101389d29307d3b
menor <josemenor@gmail.com>
2015-11-24 14:28:48 +0100

    Make imagemin only run on changed images

--- gulpfile.js/tasks/imagemin.js
+++ gulpfile.js/tasks/imagemin.js
@@ -1,16 +1,19 @@
 var browserSync = require('browser-sync');
 var config = require('../config').svg;
 var gulp = require('gulp');
+var changed = require('gulp-changed');
 var handleErrors = require('../lib/handleErrors');
 var imagemin = require('gulp-imagemin');
 
 gulp.task('imagemin', function() {
   return gulp.src(config.src)
+    .pipe(changed(config.dest))
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{
-          removeViewBox: false,
+          removeViewBox: true,
           removeDimensions: true,
+          addClassesToSVGElement: true
         }]
     }))
     .on('error', handleErrors)
