4590ddf977d6e7ff12c113695a88186893ef7b5c
menor <josemenor@gmail.com>
2015-12-10 11:27:36 +0100

    Switch node engines and change things around

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,5 +1,12 @@
 // exports.Dropdown = require('./components/Dropdown');
-import Dropdown from ('./components/Dropdown');
+// import Dropdown from ('./components/Dropdown');
+var Dropdown = require('./components/Dropdown');
 
 
-export default function(){};
+export default function(){
+  ReactDOM.render(
+    <Dropdown />,
+    document.getElementById('root')
+  );
+
+};
--- src/javascripts/components/Dropdown.js
+++ src/javascripts/components/Dropdown.js
@@ -1,20 +1,14 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-console.log('Dropdown');
 
 const Dropdown = React.createClass({
   render: function() {
     return (
       <div className="dropdown">
-        Hello, world! I am a Dropdown.
+        Hello, world! I am a Dropdow.
       </div>
     );
   }
 });
-
-ReactDOM.render(
-  <Dropdown />,
-  document.getElementById('root')
-);
 
 export default Dropdown;
