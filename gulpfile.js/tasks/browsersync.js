d45bd01b2ee1f774146be7fd46039c5d7ee14e14
menor <josemenor@gmail.com>
2015-12-14 09:57:27 +0100

    Start adding browsersync react hot loader integration Very very messy

--- gulpfile.js/tasks/browsersync.js
+++ gulpfile.js/tasks/browsersync.js
@@ -1,7 +1,35 @@
+var path = require('path');
 var browserSync = require('browser-sync');
 var gulp = require('gulp');
-var config = require('../config').browserSync;
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+
+var webpackConfig = require(path.resolve('webpack.config.js'));
+var bundler = webpack(webpackConfig);
 
 gulp.task('browserSync', function() {
-  browserSync(config);
+  browserSync({
+    server: {
+      baseDir: 'build'
+    },
+    files: ['build/**/*.html'],
+    browser: 'google chrome canary',
+    middleware: [
+        webpackDevMiddleware(bundler, {
+          // IMPORTANT: dev middleware can't access config, so we should
+          // provide publicPath by ourselves
+          publicPath: webpackConfig.output.publicPath,
+
+          // pretty colored output
+          stats: { colors: true }
+
+          // for other settings see
+          // http://webpack.github.io/docs/webpack-dev-middleware.html
+        }),
+
+        // bundler should be the same as above
+        webpackHotMiddleware(bundler)
+      ]
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/javascripts/app.js",
+    entry: [
+      'webpack/hot/dev-server',
+      'webpack-hot-middleware/client',
+      './src/javascripts/app.js',
+    ],
     output: {
       path: path.join(__dirname, '/build/'),
       filename: 'main.js'
     },
     devtool: 'source-map',
     debug: true,
+    plugins: [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ],
     module: {
       loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "react-hot"
+        },
         {
           test: /\.jsx?$/,
           exclude: /(node_modules|bower_components)/,
