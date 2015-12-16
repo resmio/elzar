aacf61ace3e5e93d18ffce06cc89386bc4bc4101
menor <josemenor@gmail.com>
2015-12-16 09:42:43 +0100

    Add a test structure and a passing test. Still no react in the mix, probably need to pass it to the babel interpreter as a loader

---
+++ src/javascripts/__tests__/Dropdown-test.js
@@ -0,0 +1,15 @@
+import test from 'tape';
+// import Dropdown from '../components/Dropdown.jsx';
+// import React from 'react/addons';
+// const {TestUtils} = React.addons;
+
+test(
+  'Passing test',
+  (assert) => {
+    assert.equal(
+      true,
+      true
+    );
+    assert.end();
+  }
+);
