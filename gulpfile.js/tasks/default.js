c75c4029c3098953b3e549514759256f351bd72c
menor <josemenor@gmail.com>
2016-01-26 10:49:59 +0100

    Add a sass:dev gulp task

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['html', 'sass', 'svgsymbol', 'webpack:development']);
+gulp.task('default', ['html', 'sass:prod', 'svgsymbol', 'webpack:development']);
